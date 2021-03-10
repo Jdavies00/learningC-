2d8b16d3f5793af95ce11b1a21806bec51e061a0
James Davies <j.davies2099@gmail.com>
2021-03-10 12:39:31 -0500

    first C++ push

---
+++ firstC++/helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//^allows me to use cout 
+using namespace std;
+//^allows me to use names form the standard libary?
+int main()
+{
+    cout << "Hello World!";
+    //cout object used with the << operator is used to out put values/ print text
+    return 0;
+}
